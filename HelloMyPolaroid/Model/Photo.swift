0c462250b013cefea264de4c88db2801eaf914aa
Daeyun <sweet_ray@naver.com>
2024-07-24 22:48:08 +0900

    Feat: topic’s photos API 모델 설계

---
+++ HelloMyPolaroid/Model/Photo.swift
@@ -0,0 +1,48 @@
+//
+//  Photo.swift
+//  HelloMyPolaroid
+//
+//  Created by 권대윤 on 7/24/24.
+//
+
+import Foundation
+
+struct Photo: Decodable {
+    
+    let id: String
+    let createdAt: String
+    let width: Int
+    let height: Int
+    let urls: Urls
+    let likes: Int
+    let user: User
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case createdAt = "created_at"
+        case width
+        case height
+        case urls
+        case likes
+        case user
+    }
+}
+
+struct Urls: Decodable {
+    let raw: String
+    let small: String
+}
+
+struct User: Decodable {
+    let name: String
+    let profileImage: ProfileImage
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case profileImage = "profile_image"
+    }
+}
+
+struct ProfileImage: Codable {
+    let small, medium, large: String
+}
